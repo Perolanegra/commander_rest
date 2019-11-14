04e5f77ae6d6d039f90336e4fe36533a26ad69d7
Perolanegra <pedratto3@gmail.com>
2019-11-13 22:09:36 -0300

    Remocao Cors Options

--- src/index.js
+++ src/index.js
@@ -20,25 +20,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-const allowedOrigins = [
-    'capacitor://localhost',
-    'ionic://localhost',
-    'http://localhost',
-    'http://localhost:8080',
-    'http://localhost:8100'
-];
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Origin not allowed by CORS'));
-        }
-    }
-}
-
-app.options('*', cors(corsOptions));
-
 // require('./controllers/ctrl')(app);
 app.listen(3500, _ => console.log('listening to 3500'));
